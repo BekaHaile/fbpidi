209484692ff34ae9d76fa83957bfde8e05154e78
Beka Haile <49582830+BekaHaile@users.noreply.github.com>
2021-02-16 15:26:48 +0300

    Product list builder extracted, scroll problem in homepage fixed

--- lib/widgets/screens/home_page.dart
+++ lib/widgets/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:fbpidi/widgets/components/fbpidi_drawer.dart';
+import 'package:fbpidi/widgets/screens/products.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -58,7 +59,7 @@ class _HomePageState extends State<HomePage> {
           _buildHorizontalList(
               "Investment Opportunities", Color.fromRGBO(230, 221, 216, 1)),
           _buildHorizontalList("Researches", Color.fromRGBO(217, 226, 241, 1)),
-          _buildProductList(),
+          ProductsPage(),
           _buildBottomCards(),
         ],
       )),
@@ -197,135 +198,6 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-    );
-  }
-
-  Widget _buildProductList() {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-          child: Row(
-            children: <Widget>[
-              Text(
-                "Products",
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              Spacer(),
-              InkWell(
-                onTap: () {
-                  print("hello");
-                },
-                child: Text("More"),
-              ),
-            ],
-          ),
-        ),
-        Container(
-          height: 1730,
-          padding: EdgeInsets.symmetric(vertical: 1.0),
-          child: GridView.builder(
-            physics: ScrollPhysics(),
-            scrollDirection: Axis.vertical,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 5.0,
-                childAspectRatio: 0.58),
-            itemBuilder: (_, int index) {
-              return Card(
-                color: Colors.white,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: Image.network(
-                        "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/body-image/public/1-corvette-stingray-c8-2019-fd-hr-hero-front_0.jpg?itok=SEYe_vLy",
-                        height: 150,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: Container(
-                        color: Colors.grey,
-                        child: Padding(
-                          padding: const EdgeInsets.all(3.0),
-                          child: Text(
-                            'Product Category',
-                            softWrap: true,
-                            style: TextStyle(color: Colors.white, fontSize: 16),
-                            textAlign: TextAlign.left,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 18.0, right: 5),
-                      child: Text(
-                        'Tomato Paste for Benin 2200g Tomato Paste ...',
-                        style: TextStyle(
-                          fontSize: 17.0,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10.0),
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.5,
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(5.0),
-                              child: Image.network(
-                                "https://media.istockphoto.com/photos/oil-refinery-chemical-petrochemical-plant-picture-id932140864?k=6&m=932140864&s=612x612&w=0&h=UujqNqhSsXBOHMd2x-X3YmkMOBLv7g1FZCCr52rC6b4=",
-                                height: 20,
-                              ),
-                            ),
-                            Column(
-                              children: [
-                                Text(
-                                  'Manufacturer Company',
-                                  softWrap: true,
-                                  style: TextStyle(
-                                      color: Colors.black, fontSize: 11),
-                                  textAlign: TextAlign.left,
-                                ),
-                                Text(
-                                  'Phone Number',
-                                  softWrap: true,
-                                  style: TextStyle(
-                                      color: Colors.black, fontSize: 11),
-                                  textAlign: TextAlign.left,
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(3.0),
-                      child: Text(
-                        'Product Category',
-                        softWrap: true,
-                        style: TextStyle(color: Colors.white, fontSize: 18),
-                        textAlign: TextAlign.left,
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            },
-            itemCount: 10,
-          ),
-        ),
-      ],
     );
   }
 
