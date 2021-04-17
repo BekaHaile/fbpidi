7b3fddbebc5f244b01bb55c7229ed4c83f752191
Beka Haile <barbeka.tola@gmail.com>
2021-04-17 11:52:37 +0300

    Product detail from home page done

--- lib/widgets/screens/home_page.dart
+++ lib/widgets/screens/home_page.dart
@@ -411,7 +411,6 @@ class _HomePageState extends State<HomePage> {
     return FutureBuilder<List<Product>>(
       future: CompanyAndProductAPI().getProductsByMainCategory("all"),
       builder: (BuildContext context, snapshot) {
-        // _fetchLanguage(context);
         if (!snapshot.hasData)
           return Center(
             child: Padding(
@@ -439,85 +438,92 @@ class _HomePageState extends State<HomePage> {
                     mainAxisSpacing: 5.0,
                     childAspectRatio: 0.58),
                 itemBuilder: (_, int index) {
-                  return Card(
-                    color: Colors.white,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(5.0),
-                          child: Image.network(
-                            "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/body-image/public/1-corvette-stingray-c8-2019-fd-hr-hero-front_0.jpg?itok=SEYe_vLy",
-                            height: 150,
+                  return InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/productDetail',
+                          arguments: {"id": products[index].id});
+                    },
+                    child: Card(
+                      color: Colors.white,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Image.network(
+                              "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/body-image/public/1-corvette-stingray-c8-2019-fd-hr-hero-front_0.jpg?itok=SEYe_vLy",
+                              height: 150,
+                            ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8.0),
-                          child: Container(
-                            color: Colors.grey,
-                            child: Padding(
-                              padding: const EdgeInsets.all(3.0),
-                              child: Text(
-                                products[index].categoryName,
-                                softWrap: true,
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 16),
-                                textAlign: TextAlign.left,
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: Container(
+                              color: Colors.grey,
+                              child: Padding(
+                                padding: const EdgeInsets.all(3.0),
+                                child: Text(
+                                  products[index].categoryName,
+                                  softWrap: true,
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 16),
+                                  textAlign: TextAlign.left,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 18.0, right: 5),
-                          child: Text(
-                            products[index].name,
-                            style: TextStyle(
-                              fontSize: 17.0,
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(left: 18.0, right: 5),
+                            child: Text(
+                              products[index].name,
+                              style: TextStyle(
+                                fontSize: 17.0,
+                              ),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 10.0),
-                          child: Container(
-                            width: MediaQuery.of(context).size.width * 0.5,
-                            child: Row(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(5.0),
-                                  child: Image.network(
-                                    "https://media.istockphoto.com/photos/oil-refinery-chemical-petrochemical-plant-picture-id932140864?k=6&m=932140864&s=612x612&w=0&h=UujqNqhSsXBOHMd2x-X3YmkMOBLv7g1FZCCr52rC6b4=",
-                                    height: 20,
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10.0),
+                            child: Container(
+                              width: MediaQuery.of(context).size.width * 0.5,
+                              child: Row(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(5.0),
+                                    child: Image.network(
+                                      "https://media.istockphoto.com/photos/oil-refinery-chemical-petrochemical-plant-picture-id932140864?k=6&m=932140864&s=612x612&w=0&h=UujqNqhSsXBOHMd2x-X3YmkMOBLv7g1FZCCr52rC6b4=",
+                                      height: 20,
+                                    ),
                                   ),
-                                ),
-                                Column(
-                                  children: [
-                                    Text(
-                                      'Manufacturer Company',
-                                      softWrap: true,
-                                      style: TextStyle(
-                                          color: Colors.black, fontSize: 11),
-                                      textAlign: TextAlign.left,
-                                    ),
-                                    Text(
-                                      'Phone Number',
-                                      softWrap: true,
-                                      style: TextStyle(
-                                          color: Colors.black, fontSize: 11),
-                                      textAlign: TextAlign.left,
-                                    ),
-                                  ],
-                                ),
-                              ],
+                                  Column(
+                                    children: [
+                                      Text(
+                                        'Manufacturer Company',
+                                        softWrap: true,
+                                        style: TextStyle(
+                                            color: Colors.black, fontSize: 11),
+                                        textAlign: TextAlign.left,
+                                      ),
+                                      Text(
+                                        'Phone Number',
+                                        softWrap: true,
+                                        style: TextStyle(
+                                            color: Colors.black, fontSize: 11),
+                                        textAlign: TextAlign.left,
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   );
                 },
