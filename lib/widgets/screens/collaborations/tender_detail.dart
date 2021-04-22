48f54136cf51babfe572980c052e09a619d5f677
Beka Haile <barbeka.tola@gmail.com>
2021-04-22 19:00:21 +0300

    Apply for tender removed

--- lib/widgets/screens/collaborations/tender_detail.dart
+++ lib/widgets/screens/collaborations/tender_detail.dart
@@ -146,42 +146,43 @@ class _TenderDetailState extends State<TenderDetail> {
                                       height: 10.0,
                                     ),
                                     Padding(
-                                      padding:
-                                          const EdgeInsets.only(left: 20.0),
-                                      child: tender.status == "Upcoming"
-                                          ? Text(
-                                              "This tender is now in Upcoming status. So, you can't participate in this tender.",
-                                              style: TextStyle(
-                                                  color: Colors.red,
-                                                  fontSize: 17),
-                                            )
-                                          : tender.tenderType == "Paid"
-                                              ? Text(
-                                                  "This Tender is not Free, You can read the description below and attend the tender in person!",
-                                                  style: TextStyle(
-                                                      color: Colors.red,
-                                                      fontSize: 17),
-                                                )
-                                              : ElevatedButton(
-                                                  onPressed: () {
-                                                    print(
-                                                        "Being clicked to open a modal");
-                                                    _openParticipateModal(
-                                                        context, tender.id);
-                                                  },
-                                                  style:
-                                                      ElevatedButton.styleFrom(
-                                                    primary: Theme.of(context)
-                                                        .buttonColor,
-                                                  ),
-                                                  child: Text(
-                                                    "Participate in Tender",
+                                        padding:
+                                            const EdgeInsets.only(left: 20.0),
+                                        child: tender.status == "Upcoming"
+                                            ? Text(
+                                                "This tender is now in Upcoming status. So, you can't participate in this tender.",
+                                                style: TextStyle(
+                                                    color: Colors.red,
+                                                    fontSize: 17),
+                                              )
+                                            : tender.tenderType == "Paid"
+                                                ? Text(
+                                                    "This Tender is not Free, You can read the description below and attend the tender in person!",
                                                     style: TextStyle(
-                                                        color: Colors.white,
+                                                        color: Colors.red,
                                                         fontSize: 17),
-                                                  ),
-                                                ),
-                                    ),
+                                                  )
+                                                : Container()
+                                        // ElevatedButton(
+                                        //     onPressed: () {
+                                        //       print(
+                                        //           "Being clicked to open a modal");
+                                        //       _openParticipateModal(
+                                        //           context, tender.id);
+                                        //     },
+                                        //     style:
+                                        //         ElevatedButton.styleFrom(
+                                        //       primary: Theme.of(context)
+                                        //           .buttonColor,
+                                        //     ),
+                                        //     child: Text(
+                                        //       "Participate in Tender",
+                                        //       style: TextStyle(
+                                        //           color: Colors.white,
+                                        //           fontSize: 17),
+                                        //     ),
+                                        //   ),
+                                        ),
                                     SizedBox(
                                       height: 15.0,
                                     ),
@@ -266,127 +267,127 @@ class _TenderDetailState extends State<TenderDetail> {
     );
   }
 
-  _openParticipateModal(context, tenderId) {
-    TextEditingController firstController = TextEditingController(),
-        lastController = TextEditingController(),
-        companyController = TextEditingController(),
-        tinController = TextEditingController(),
-        emailController = TextEditingController(),
-        phoneController = TextEditingController();
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return Padding(
-            padding: const EdgeInsets.only(left: 0.0, right: 0.0),
-            child: AlertDialog(
-              scrollable: true,
-              content: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    height: 3,
-                    color: Theme.of(context).scaffoldBackgroundColor,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  _buildTextField("First Name", firstController),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  _buildTextField("Last Name", lastController),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  _buildTextField("Company Name", companyController),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  _buildTextField("Company tin Number", tinController),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  _buildTextField("Email Address", emailController),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  _buildTextField("Phone Number", phoneController),
-                ],
-              ),
-              actions: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(primary: Colors.red),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text(
-                    'Cancel',
-                    style: TextStyle(fontSize: 17),
-                  ),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(primary: Colors.green),
-                  onPressed: () async {
-                    Map<String, dynamic> applyData = {
-                      "first_name": firstController.text,
-                      "last_name": lastController.text,
-                      "phone_number": phoneController.text,
-                      "email": emailController.text,
-                      "company_name": companyController.text,
-                      "comany_tin_number": tinController.text,
-                      "tender": tenderId,
-                    };
-                    await CollaborationsApi()
-                        .participateInTender(applyData)
-                        .then((value) {
-                      print(value);
-                      Navigator.pop(context);
-                    });
-                  },
-                  child: Text(
-                    'Submit',
-                    style: TextStyle(fontSize: 17),
-                  ),
-                ),
-              ],
-            ),
-          );
-        });
-  }
+  // _openParticipateModal(context, tenderId) {
+  //   TextEditingController firstController = TextEditingController(),
+  //       lastController = TextEditingController(),
+  //       companyController = TextEditingController(),
+  //       tinController = TextEditingController(),
+  //       emailController = TextEditingController(),
+  //       phoneController = TextEditingController();
+  //   showDialog(
+  //       context: context,
+  //       builder: (BuildContext context) {
+  //         return Padding(
+  //           padding: const EdgeInsets.only(left: 0.0, right: 0.0),
+  //           child: AlertDialog(
+  //             scrollable: true,
+  //             content: Column(
+  //               crossAxisAlignment: CrossAxisAlignment.start,
+  //               children: [
+  //                 SizedBox(
+  //                   height: 10,
+  //                 ),
+  //                 Container(
+  //                   height: 3,
+  //                   color: Theme.of(context).scaffoldBackgroundColor,
+  //                 ),
+  //                 SizedBox(
+  //                   height: 10,
+  //                 ),
+  //                 _buildTextField("First Name", firstController),
+  //                 SizedBox(
+  //                   height: 10,
+  //                 ),
+  //                 _buildTextField("Last Name", lastController),
+  //                 SizedBox(
+  //                   height: 10,
+  //                 ),
+  //                 _buildTextField("Company Name", companyController),
+  //                 SizedBox(
+  //                   height: 10,
+  //                 ),
+  //                 _buildTextField("Company tin Number", tinController),
+  //                 SizedBox(
+  //                   height: 10,
+  //                 ),
+  //                 _buildTextField("Email Address", emailController),
+  //                 SizedBox(
+  //                   height: 10,
+  //                 ),
+  //                 _buildTextField("Phone Number", phoneController),
+  //               ],
+  //             ),
+  //             actions: [
+  //               ElevatedButton(
+  //                 style: ElevatedButton.styleFrom(primary: Colors.red),
+  //                 onPressed: () {
+  //                   Navigator.pop(context);
+  //                 },
+  //                 child: Text(
+  //                   'Cancel',
+  //                   style: TextStyle(fontSize: 17),
+  //                 ),
+  //               ),
+  //               ElevatedButton(
+  //                 style: ElevatedButton.styleFrom(primary: Colors.green),
+  //                 onPressed: () async {
+  //                   Map<String, dynamic> applyData = {
+  //                     "first_name": firstController.text,
+  //                     "last_name": lastController.text,
+  //                     "phone_number": phoneController.text,
+  //                     "email": emailController.text,
+  //                     "company_name": companyController.text,
+  //                     "comany_tin_number": tinController.text,
+  //                     "tender": tenderId,
+  //                   };
+  //                   await CollaborationsApi()
+  //                       .participateInTender(applyData)
+  //                       .then((value) {
+  //                     print(value);
+  //                     Navigator.pop(context);
+  //                   });
+  //                 },
+  //                 child: Text(
+  //                   'Submit',
+  //                   style: TextStyle(fontSize: 17),
+  //                 ),
+  //               ),
+  //             ],
+  //           ),
+  //         );
+  //       });
+  // }
 
-  Widget _buildTextField(String title, TextEditingController controller) {
-    return Theme(
-      data: new ThemeData(
-        primaryColor: Theme.of(context).buttonColor,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            title + ": ",
-            style: TextStyle(fontSize: 17),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.95,
-            child: TextField(
-              decoration: new InputDecoration(
-                border: new OutlineInputBorder(
-                    borderSide:
-                        new BorderSide(color: Theme.of(context).buttonColor)),
-                hintText: title,
-                labelText: title,
-              ),
-              controller: controller,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+  // Widget _buildTextField(String title, TextEditingController controller) {
+  //   return Theme(
+  //     data: new ThemeData(
+  //       primaryColor: Theme.of(context).buttonColor,
+  //     ),
+  //     child: Column(
+  //       crossAxisAlignment: CrossAxisAlignment.start,
+  //       children: [
+  //         Text(
+  //           title + ": ",
+  //           style: TextStyle(fontSize: 17),
+  //         ),
+  //         SizedBox(
+  //           height: 5,
+  //         ),
+  //         Container(
+  //           width: MediaQuery.of(context).size.width * 0.95,
+  //           child: TextField(
+  //             decoration: new InputDecoration(
+  //               border: new OutlineInputBorder(
+  //                   borderSide:
+  //                       new BorderSide(color: Theme.of(context).buttonColor)),
+  //               hintText: title,
+  //               labelText: title,
+  //             ),
+  //             controller: controller,
+  //           ),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
 }
